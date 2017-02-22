7a60ac11fe8a2440013b02fdb3a77851fffb59dd
Mandy Fung <frozenera@hotmail.com>
2017-02-21 17:58:22 -0800

    Mock categories request form

--- scripts/js/content.jsx
+++ scripts/js/content.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import request from 'superagent';
 import autobind from 'class-autobind';
 import Alerts from "./alerts.jsx";
-import {Grid} from 'react-bootstrap';
 import {Row} from 'react-bootstrap';
 import {Col} from 'react-bootstrap';
 import {Form} from 'react-bootstrap';
 import {FormGroup} from 'react-bootstrap';
 import {ControlLabel} from 'react-bootstrap';
-import {FormControl} from 'react-bootstrap';
-import {Checkbox} from 'react-bootstrap';
-import {Button} from 'react-bootstrap';
 import {PageHeader} from 'react-bootstrap';
 import {Radio} from 'react-bootstrap';
-import {HelpBlock} from 'react-bootstrap';
 import ProfileRequestForm from "./profilerequestform.jsx";
 import CategoriesRequestForm from "./categoriesrequestform.jsx";
 
@@ -23,34 +17,9 @@ export default class RequestForm extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            statusDisplay : false,
-            statusMessage : null,
             selectedOption : null
         }
         autobind(this);
-    }
-
-    callRequest(e) {
-        e.preventDefault();
-        request
-            .post("/requests")
-            .send({ "category_id": 11})
-            .end((err, res) => {
-                console.log(res);
-                if (err){
-                    this.setState({
-                        statusMessage: "noConnection",
-                        statusDisplay: true
-                    });
-                }
-                else{
-                    this.setState({
-                        statusMessage: 'success',
-                        statusDisplay: true
-
-                    });
-                }
-            });
     }
 
     handleOptionChange(e) {
@@ -59,19 +28,6 @@ export default class RequestForm extends React.Component {
 
     render() {
             var form;
-            var alert;
-            var description;
-            var submit;
-
-            if (this.state.selectedOption !== null) {
-              description = <Col>
-                <ControlLabel><b>Description</b></ControlLabel>
-                <HelpBlock>Enter an optional short description about your request (max 250 characters).</HelpBlock>
-                <FormControl componentClass="textarea" placeholder="Description" maxLength='250'/></Col>;
-              submit = <Button onSubmit = {this.callRequest}  type="submit" disabled={true}>
-                    Submit Request
-                </Button>;
-            }
 
             if (this.state.selectedOption === 'categories') {
               form = <CategoriesRequestForm/>
@@ -79,17 +35,12 @@ export default class RequestForm extends React.Component {
               form = <ProfileRequestForm/>
             }
 
-            if (this.state.statusDisplay === true) {
-                alert = <Alerts title={this.state.statusMessage}/>
-            }
-
             return (
                 <div>
                   <PageHeader>Create a Request</PageHeader>
-                {alert}
+                <ControlLabel><b>Select a Product Type</b></ControlLabel>
                 <Form horizontal>
                     <FormGroup controlId="formControlsSelect">
-                        <Col smOffset={1}><ControlLabel><b>Select a Product Type</b></ControlLabel></Col>
                           <FormGroup>
                             <Row>
                             <Col smOffset={4} sm={3}>
@@ -105,15 +56,10 @@ export default class RequestForm extends React.Component {
                             </Row>
                           </FormGroup>
 
-                          <Col smOffset={1} sm={10}>
-                          {form}
-                          <p/>
-                          {description}
-                          <p/>
-                          {submit}
-                          </Col>
                     </FormGroup>
                 </Form>
+
+                {form}
                 </div>
             );
     }
