4df13a0dc7e76344e746d0ddda16a43cc7729dc7
nanstc <nanstchen@gmail.com>
2017-02-21 17:10:55 -0800

    design and implement log in system - playing around with auth token using different libraries. Code does not work atm

--- src/main/java/com/coastcapitalsavings/RequisitioningApplication.java
+++ src/main/java/com/coastcapitalsavings/RequisitioningApplication.java
@@ -3,12 +3,18 @@ package com.coastcapitalsavings;
 import com.coastcapitalsavings.datasource.DatasourceInfo;
 import com.coastcapitalsavings.datasource.IDataSourceInfoParser;
 import com.coastcapitalsavings.datasource.JSONDataSourceInfoParser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import javax.sql.DataSource;
 import java.io.IOException;
@@ -36,5 +42,29 @@ public class RequisitioningApplication {
 	}
 
 
+	@Configuration
+    @EnableWebSecurity
+    public static class WebSecurityConfigController extends WebSecurityConfigurerAdapter {
 
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
+                        .anyRequest().authenticated()
+                        .and()
+                        .formLogin()
+                    .loginPage("/login")
+                        .permitAll()
+                        .and()
+                    .logout()
+                        .permitAll();
+        }
+
+        @Autowired
+        public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+            auth
+                    .inMemoryAuthentication()
+                    .withUser("user").password("password").roles("USER");
+        }
+    }
 }
