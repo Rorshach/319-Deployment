172ae93ff5ef97c3015403f1da02a181abf8e343
Alek Hrycaiko <alekhrycaiko@gmail.com>
2017-02-22 18:21:14 -0800

    basic outline of what happen for history table display

--- scripts/js/history_table.jsx
+++ scripts/js/history_table.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import {Table} from 'reactable';
+import {request} from 'superagent';
 export default class HistoryTable extends React.Component {
     constructor(props){
         super(props);
+        this.state = {data : null}
+    }
+
+    componentWillMount(){
+        request
+            .get("/requests")
+            .end(function(err, res){
+                // process the data.
+                this.setState({data : res});
+                // set the data into state.
+            });
+    }
+
+    getTableData(){
+        return [
+            { Name: 'Griffin Smith', Age: 18 },
+            { Age: 23,  Name: 'Lee Salminen' },
+            { Age: 28, Position: 'Developer' },
+        ];
     }
 
     render() {
         return (
-            <Table className="history-table" data={[
-                { Name: 'Griffin Smith', Age: 18 },
-                { Age: 23,  Name: 'Lee Salminen' },
-                { Age: 28, Position: 'Developer' },
-            ]}>
+            <Table className="history-table" data={this.getTableData()}>
             </Table>
         )
     }
