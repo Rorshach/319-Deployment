dbcbfc1c49f8fa443170f642324cadf770bb07f8
Alek Hrycaiko <alekhrycaiko@gmail.com>
2017-02-22 18:38:34 -0800

    more setup with table and pre-modal implementation

--- scripts/js/history_table.jsx
+++ scripts/js/history_table.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Table} from 'reactable';
+import {Table, Tr} from 'reactable';
 import {request} from 'superagent';
 export default class HistoryTable extends React.Component {
     constructor(props){
         super(props);
         this.state = {data : null}
     }
-
+    /*
     componentWillMount(){
         request
             .get("/requests")
@@ -16,18 +16,50 @@ export default class HistoryTable extends React.Component {
                 // set the data into state.
             });
     }
+    */
 
     getTableData(){
         return [
             { Name: 'Griffin Smith', Age: 18 },
             { Age: 23,  Name: 'Lee Salminen' },
             { Age: 28, Position: 'Developer' },
+            { Age: 23,  Name: 'Lee Salminen' },
+            { Age: 23,  Name: 'Lee Salminen' },
+            { Age: 23,  Name: 'Lee Salminen' },
+            { Age: 23,  Name: 'Lee Salminen' },
+            { Age: 23,  Name: 'Lee Salminen' },
+            { Age: 23,  Name: 'Lee Salminen' },
+            { Age: 23,  Name: 'Lee Salminen' },
+            { Age: 23,  Name: 'Lee Salminen' },
+            { Age: 23,  Name: 'Lee Salminen' },
+            { Age: 23,  Name: 'Lee Salminen' },
+            { Age: 23,  Name: 'Lee Salminen' },
+            { Age: 23,  Name: 'Lee Salminen' },
+            { Age: 23,  Name: 'Lee Salminen' },
+            { Age: 23,  Name: 'Lee Salminen' },
+            { Age: 23,  Name: 'Lee Salminen' },
+            { Age: 23,  Name: 'Lee Salminen' },
+            { Age: 23,  Name: 'Lee Salminen' },
+            { Age: 23,  Name: 'Lee Salminen' },
+            { Age: 23,  Name: 'Lee Salminen' },
+            { Age: 23,  Name: 'Lee Salminen' },
+
         ];
     }
-
+    Modal(){
+        console.log("Hello i am le modal");
+    }
+    // TODO: Need to have real data to work with in order to finalie how the column/rows will appear.
+    // TODO: Need to do styling.
+    // TODO: Need to handle opening the modal or the page on click.
+    // TODO: Need to handle having an appropriate cursor for clicking a link versus sorting. Might be better to have
+    // its own column with a button
     render() {
         return (
-            <Table className="history-table" data={this.getTableData()}>
+            <Table className="table hover" data={this.getTableData()}
+                   sortable={true} itemsPerPage={10} pageButtonLimit={5} >
+                <Tr onClick={this.Modal} className="hover"
+                    data={{ name: 'Other Row' , content: 'This is a different row' }} />
             </Table>
         )
     }
